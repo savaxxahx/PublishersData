eb813d845fd17f94a2209d4be54a1e4a0426f969
savaxxahx <savannah@katherynsavannah.com>
2017-11-16 09:56:30 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,6 +10,8 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 def render_main():
     return render_template('home.html')
 
+
+@app.route("/comparison")
 def compare_data():
       with open('publishers.json') as publishers_data:
         books = json.load(publishers_data)
