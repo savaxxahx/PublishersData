a4a1a5e9f7f304d531b9a92c17e39e0ab32d65ff
user <user>
2017-12-01 10:00:39 -0800

    trying to work on graph

---
+++ graph.js
@@ -0,0 +1,76 @@
+function BarGraph(ctx) {
+
+  // Private properties and methods
+	
+  var that = this;
+  var startArr;
+  var endArr;
+ 
+ 
+ // Public properties and methods
+	
+  this.width = 300;
+  this.height = 150;	
+  this.maxValue;
+  this.margin = 5;
+  this.colors = ["purple", "red", "green", "yellow"];
+  this.curArr = [];
+  this.backgroundColor = "#fff";
+  this.xAxisLabelArr = [];
+  this.yAxisLabelArr = [];
+  this.animationInterval = 100;
+  this.animationSteps = 10;
+	
+	
+	
+	var draw = function (arr) {
+							
+	  var numOfBars = arr.length;
+	  var barWidth;
+	  var barHeight;
+	  var border = 2;
+	  var ratio;
+	  var maxBarHeight;
+	  var gradient;
+	  var largestValue;
+	  var graphAreaX = 0;
+	  var graphAreaY = 0;
+	  var graphAreaWidth = that.width;
+	  var graphAreaHeight = that.height;
+	  var i;
+	  
+	  ctx.fillStyle = that.backgroundColor;
+	  ctx.fillRect(0, 0, that.width, that.height);
+					
+	  // If x axis labels exist then make room	
+	  if (that.xAxisLabelArr.length) {
+		graphAreaHeight -= 40;
+	  }
+	  
+	  
+	  
+	  // Calculate dimensions of the bar
+	  barWidth = graphAreaWidth / numOfBars - that.margin * 2;
+	  maxBarHeight = graphAreaHeight - 25;
+				
+	  // Determine the largest value in the bar array
+	  var largestValue = 0;
+	  for (i = 0; i < arr.length; i += 1) {
+		if (arr[i] > largestValue) {
+		  largestValue = arr[i];	
+		}
+	  }
+	  
+	  // For each bar
+	  for (i = 0; i < arr.length; i += 1) {
+		// Set the ratio of current bar compared to the maximum
+		if (that.maxValue) {
+		  ratio = arr[i] / that.maxValue;
+		} else {
+		  ratio = arr[i] / largestValue;
+		}
+		
+		barHeight = ratio * maxBarHeight;
+  
+  
+  
